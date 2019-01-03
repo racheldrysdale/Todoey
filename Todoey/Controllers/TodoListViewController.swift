bbd370170d5e2b7924850c47fe812acd489fe40a
Rachel Drysdale <racheldrysdale@msn.com>
2019-01-03 15:48:56 +0000

    Convert to MVC design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 	
-	var itemArray = ["Year at a glance", "Future Log", "2019 Goals", "Finances"]
+	var itemArray = [Item]()
 	
 	let defaults = UserDefaults.standard
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+		
+		let newItem = Item()
+		newItem.title = "Year at a Glance"
+		itemArray.append(newItem)
+		
+		let newItem2 = Item()
+		newItem2.title = "Future Log"
+		itemArray.append(newItem2)
+		
+		let newItem3 = Item()
+		newItem3.title = "2019 Goals"
+		itemArray.append(newItem3)
+		
+		if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 			itemArray = items
 		}
 	}
@@ -32,21 +44,19 @@ class TodoListViewController: UITableViewController {
 	// Create a cell and label it with the text from the corresponding item in itemArray
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-		cell.textLabel?.text = itemArray[indexPath.row]
+		let item = itemArray[indexPath.row]
+		cell.textLabel?.text = item.title
+		// Ternary operator - change cell accessory type depending on if item.done is true; if true, set accessory type to checkmark, if false, set accessory type to none
+		cell.accessoryType = item.done ? .checkmark : .none
 		return cell
 	}
 	
 	// MARK: - TableView Delegate Methods
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		// print(itemArray[indexPath.row])
-		
 		// If row doesn't have a checkmark then add a checkmark when tapped, if row does have a checkmark then remove checkmark when tapped
-		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .none
-		} else {
-			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-		}
+		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+		tableView.reloadData()
 		
 		// Animates table view so that when you tap on item it highlights grey briefly but doesn't stay grey as before
 		tableView.deselectRow(at: indexPath, animated: true)
@@ -63,7 +73,9 @@ class TodoListViewController: UITableViewController {
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 			// What will happen once the user clicks the Add Item button on our UIAlert
 			// Add item to item array
-			self.itemArray.append(textField.text!)
+			let newItem = Item()
+			newItem.title = textField.text!
+			self.itemArray.append(newItem)
 			// Save item array into defaults
 			self.defaults.set(self.itemArray, forKey: "TodoListArray")
 			// At this point even though we have added the item to itemArray, we have not refreshed the view and so the item won't show in the table view, so we need to reload the data
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Rachel Drysdale on 03/01/2019.
+//  Copyright © 2019 Rachel Drysdale. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+	
+	var title: String = ""
+	var done: Bool = false
+
+}
