5a319e80b43f6b215b8d4b3fd9c9fe0e211e6aac
Rachel Drysdale <racheldrysdale@msn.com>
2019-01-04 15:09:48 +0000

    Persisting data using CoreData

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 	
 	var itemArray = [Item]()
 	
-	let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+	var selectedCategory: Category? {
+		didSet {
+			loadItems()
+		}
+	}
+
+	let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		print(dataFilePath)
+		print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 		
-		loadItems()
-
 	}
 	
 	// MARK: - TableView Datasource Methods
@@ -44,6 +49,10 @@ class TodoListViewController: UITableViewController {
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		// If row doesn't have a checkmark then add a checkmark when tapped, if row does have a checkmark then remove checkmark when tapped
+		
+//		context.delete(itemArray[indexPath.row])
+//		itemArray.remove(at: indexPath.row)
+		
 		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 		saveItems()
 		// Animates table view so that when you tap on item it highlights grey briefly but doesn't stay grey as before
@@ -61,8 +70,10 @@ class TodoListViewController: UITableViewController {
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 			// What will happen once the user clicks the Add Item button on our UIAlert
 			// Add item to item array
-			let newItem = Item()
+			let newItem = Item(context: self.context)
 			newItem.title = textField.text!
+			newItem.done = false
+			newItem.parentCategory = self.selectedCategory
 			self.itemArray.append(newItem)
 			// Save item array into defaults
 			self.saveItems()
@@ -82,27 +93,50 @@ class TodoListViewController: UITableViewController {
 	// MARK: - Model Manipulation Methods
 	
 	func saveItems() {
-		let encoder = PropertyListEncoder()
 		do {
-			let data = try encoder.encode(itemArray)
-			try data.write(to: dataFilePath!)
+			try context.save()
 		} catch {
-			print("Error encoding item array, \(error)")
+			print ("Error saving context \(error)")
 		}
 		tableView.reloadData()
 	}
 	
-	func loadItems() {
-		if let data = try? Data(contentsOf: dataFilePath!) {
-			let decoder = PropertyListDecoder()
-			do {
-				itemArray = try decoder.decode([Item].self, from: data)
-			} catch {
-				print("Error decoding item array, \(error)")
+	func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+		let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+		if let additionalPredicate = predicate {
+			request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+		} else {
+			request.predicate = categoryPredicate
+		}
+		do {
+			itemArray = try context.fetch(request)
+		} catch {
+			print("Error fetching data from context, \(error)")
+		}
+		tableView.reloadData()
+	}
+
+}
+
+// MARK: - Search Bar Methods
+
+extension TodoListViewController: UISearchBarDelegate {
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		let request: NSFetchRequest<Item> = Item.fetchRequest()
+		let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+		request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+		
+		loadItems(with: request, predicate: predicate)
+	}
+	
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		if searchBar.text?.count == 0 {
+			loadItems()
+			DispatchQueue.main.async {
+				searchBar.resignFirstResponder()
 			}
 		}
 	}
 	
-
 }
-
